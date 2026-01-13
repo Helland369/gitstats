74727aa0addcbba76c62ead35646cac2be1c42ef
Thomas Helland <thomas_helland@pm.me>
2026-01-13 13:32:53 +0100

    Added get_github_contrib for getting contrib data from github

---
+++ github_stats/get_github_contrib.go
@@ -0,0 +1,77 @@
+package github_stats
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+const url = "https://api.github.com/graphql"
+
+type graphQLRequest struct {
+	Query string `json:"query"`
+	Variables map[string]any `json:"variables,omitempty"`
+}
+
+func Get_github_contrib(userName string) {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found!")
+	}
+
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		panic("set GITHUB_TOKE env variable to github personal access token!")
+	}
+
+	query := `
+           query($login: String!) {
+             user(login: $login) {
+               contributionsCollection {
+                 contributionCalendar {
+                   totalContributions
+                   weeks {
+                     firstDay
+                     contributionDays {
+                       date
+                       weekday
+                       contributionCount
+                       color
+                     }
+                   }
+                 }
+               }
+             }
+           }`
+
+	requBody, _ := json.Marshal(graphQLRequest {
+		Query: query,
+		Variables: map[string]any{
+			"login": userName,
+		},
+	})
+
+	req, _ := http.NewRequest("POST", url, bytes.NewReader(requBody))
+	req.Header.Set("Authorization", "Bearer " + token)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/vnd.github+json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer req.Body.Close()
+
+	b, _ := io.ReadAll(res.Body)
+	if res.StatusCode != 200 {
+		fmt.Printf("HTTP %d\n%s\n", res.StatusCode, string(b))
+		return
+	}
+
+	fmt.Println(string(b))
+}
--- main.go
+++ main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"flag"
+	
 	"github.com/Helland369/gitstats/git_stats"
+	"github.com/Helland369/gitstats/github_stats"
 )
 
 func main() {
 	var folder string
 	var email string
+	var userName string
 	flag.StringVar(&folder, "add", "", "add a new folder to scan Git repositories")
-	flag.StringVar(&email, "email", "thomas_helland@pm.me", "the email to scan")
+	flag.StringVar(&email, "email", "", "the email to scan")
+	flag.StringVar(&userName, "user", "", "github user name")
 	flag.Parse()
 
 	if folder != "" {
 		git_stats.Scan(folder)
 		return
 	}
+
+	if userName != "" {
+		github_stats.Get_github_contrib(userName)		
+	}
+	
 	git_stats.Stats(email)
 }
